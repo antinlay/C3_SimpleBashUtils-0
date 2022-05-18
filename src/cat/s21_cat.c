bae0e2fa9273c0c186052bdb5a1756d94390954c
Janiece Eleonor <janiecee@ev-j5.kzn.21-school.ru>
2022-05-18 17:33:22 +0300

    janiecee OPEN FILE

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,42 +1,45 @@
 #include <stdio.h>
 #include <string.h>
 
+#include "../s21_string.h"
+
 struct s21_cat {
   int OPT_B;
   int OPT_E;
   int OPT_S;
   int OPT_T;
   int OPT_N;
+  FILE *p;
 } p;
 
-int main(int argc, char *argv[]) {
-  // char flagList[] = "best";
+int main(int argc, char **argv) {
   if (argc > 1) {
-    size_t i = 0;
-    while (argc) {
-      if (argv[argc][i] == '-') {
-        for (i = 1; i < strlen(argv[argc]); i++) {
-          if (argv[argc][i++] == 'b')
+    size_t count = argc - 1, i = 0;
+    while (count) {
+      if (argv[count][i] == '-') {
+        for (i = 0; i < s21_strlen(argv[count]); i++) {
+          if (argv[count][i] == 'b')
             p.OPT_B = 1;
-          else if (argv[argc][i] == 'e')
+          else if (argv[count][i] == 'e')
             p.OPT_E = 1;
-          else if (argv[argc][i] == 's')
+          else if (argv[count][i] == 's')
             p.OPT_S = 1;
-          else if (argv[argc][i] == 't')
+          else if (argv[count][i] == 't')
             p.OPT_T = 1;
-          else if (argv[argc][i] == 'n')
+          else if (argv[count][i] == 'n')
             p.OPT_T = 1;
         }
-        argc--;
-
-        // if (argv[argc][1] == 'b' || argv[argc][1] == 'e' ||
-        //     argv[argc][1] == 's' || argv[argc][1] == 't') {
-        // }
+        count--;
       } else {
-        argc--;
+        char strFile[1000] = "";
+        p.p = fopen(argv[count], "r");
+        while (fgets(strFile, sizeof(strFile), p.p)) {
+          printf("%s", strFile);
+        }
+        count--;
       }
     }
-    printf("BEST: %d%d%d%d%d", p.OPT_B, p.OPT_E, p.OPT_S, p.OPT_T, p.OPT_N);
+    printf("\nBEST: %d%d%d%d%d", p.OPT_B, p.OPT_E, p.OPT_S, p.OPT_T, p.OPT_N);
   }
   return 0;
 }
