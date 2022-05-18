ecba7845580a6f8175752707c47461c3ea8c19ba
Janiece Eleonor <janiecee@ev-j5.kzn.21-school.ru>
2022-05-18 15:12:21 +0300

    janiecee ARGV ARGC

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -1,13 +1,42 @@
 #include <stdio.h>
 #include <string.h>
-#define BSIZE 100000
 
-int main(void) {
-  //   FILE *p;
-  char cmdName[] = "s21_cat ";
-  char scnName[BSIZE] = "";
-  //   char fileName[BSIZE] = "";
-  scanf("%s", scnName);
-  printf("%ld", strspn(scnName, cmdName));
+struct s21_cat {
+  int OPT_B;
+  int OPT_E;
+  int OPT_S;
+  int OPT_T;
+  int OPT_N;
+} p;
+
+int main(int argc, char *argv[]) {
+  // char flagList[] = "best";
+  if (argc > 1) {
+    size_t i = 0;
+    while (argc) {
+      if (argv[argc][i] == '-') {
+        for (i = 1; i < strlen(argv[argc]); i++) {
+          if (argv[argc][i++] == 'b')
+            p.OPT_B = 1;
+          else if (argv[argc][i] == 'e')
+            p.OPT_E = 1;
+          else if (argv[argc][i] == 's')
+            p.OPT_S = 1;
+          else if (argv[argc][i] == 't')
+            p.OPT_T = 1;
+          else if (argv[argc][i] == 'n')
+            p.OPT_T = 1;
+        }
+        argc--;
+
+        // if (argv[argc][1] == 'b' || argv[argc][1] == 'e' ||
+        //     argv[argc][1] == 's' || argv[argc][1] == 't') {
+        // }
+      } else {
+        argc--;
+      }
+    }
+    printf("BEST: %d%d%d%d%d", p.OPT_B, p.OPT_E, p.OPT_S, p.OPT_T, p.OPT_N);
+  }
   return 0;
 }
