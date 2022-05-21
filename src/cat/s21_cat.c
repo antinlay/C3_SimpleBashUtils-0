9e8ff6e2ae25c04a90a097b535f670de23c8ac0f
Janiece Eleonor <janiecee@ev-j5.kzn.21-school.ru>
2022-05-22 02:08:49 +0300

    janiecee T S EN EB not work

--- src/cat/s21_cat.c
+++ src/cat/s21_cat.c
@@ -19,33 +19,34 @@ void init() {
   p.p = NULL;
 }
 
-int main(int argc, char **argv) {
+int main(int argc, char *argv[]) {
   size_t ok = 0;
   if (argc > 1) {
     size_t count = 1, i = 0;
     while (count < (size_t)argc) {
+      // i = 0;
       if (argv[count][i] == '-') {
-        for (i = 0; i < strlen(argv[count]); i++) {
+        for (i = 0; argv[count][i] && argv[count][i] != ' '; i++) {
           switch (argv[count][i]) {
             case 'b':
               p.OPTION_B = 1;
-              argv[count] = "";
+              // argv[count] = "";
               break;
             case 'e':
               p.OPTION_E = 1;
-              argv[count] = "";
+              // argv[count] = "";
               break;
             case 's':
               p.OPTION_S = 1;
-              argv[count] = "";
+              // argv[count] = "";
               break;
             case 't':
               p.OPTION_T = 1;
-              argv[count] = "";
+              // argv[count] = "";
               break;
             case 'n':
               p.OPTION_N = 1;
-              argv[count] = "";
+              // argv[count] = "";
               break;
             default:
               break;
@@ -58,60 +59,84 @@ int main(int argc, char **argv) {
         if ((p.p = fopen(argv[count], "r")) == NULL) {
           ok = 1;
         } else {
-          if (p.OPTION_N == 1) {
-            printf("%6lu\t", n++);
-            while ((outputChar = fgetc(p.p)) != EOF) {
-              if (outputChar == '\n') {
-                printf("\n%6lu\t", n++);
-                continue;
-              }
-              printf("%c", outputChar);
-            }
-          } else if (p.OPTION_T == 1) {
-            while ((outputChar = fgetc(p.p)) != EOF) {
-              if (outputChar == '\t') {
-                printf("%c%c", '^', 'I');
-                continue;
-              }
-              printf("%c", outputChar);
-            }
-          } else if (p.OPTION_B == 1) {
+          if (p.OPTION_N == 1 && p.OPTION_B != 1) printf("%6lu\t", n++);
+          if (p.OPTION_B == 1) {
             if ((outputChar = fgetc(p.p)) == '\n') printf("\n");
             printf("%6lu\t", n++);
-            while ((outputChar = fgetc(p.p)) != EOF) {
+          }
+          if (p.OPTION_S == 1) printf("%c", outputChar);
+          while ((outputChar = fgetc(p.p)) != EOF) {
+            if (p.OPTION_N == 1 || p.OPTION_T == 1 || p.OPTION_B == 1 ||
+                p.OPTION_S == 1 || p.OPTION_E == 1) {
               if (outputChar == '\n') {
-                printf("\n");
-                while ((outputChar = fgetc(p.p)) == '\n') {
-                  printf("\n");
-                }
-                printf("%6lu\t", n++);
-              }
-              printf("%c", outputChar);
-            }
-          } else if (p.OPTION_S == 1) {
-            while ((outputChar = fgetc(p.p)) != EOF) {
-              printf("%c", outputChar);
-              if (outputChar == '\n') {
-                if ((outputChar = fgetc(p.p)) == '\n') {
-                  printf("%c", outputChar);
-                  while (outputChar == '\n') {
-                    outputChar = fgetc(p.p);
+                if (p.OPTION_E == 1 && p.OPTION_N != 1 && p.OPTION_B != 1)
+                  printf("%c\n", '$');
+                else if (p.OPTION_E == 1)
+                  printf("%c", '$');
+                if (p.OPTION_B == 1 || p.OPTION_S == 1) {
+                  if (p.OPTION_S == 1) {
+                    if ((outputChar = fgetc(p.p)) == '\n') {
+                      printf("%c", outputChar);
+                    }
                   }
-                  printf("%c", outputChar);
-                } else {
-                  printf("%c", outputChar);
+                  while ((outputChar = fgetc(p.p)) == '\n') {
+                    if (p.OPTION_B == 1) printf("%c", outputChar);
+                  }
+                  if (p.OPTION_S == 1) printf("\n%c", outputChar);
                 }
+                if (p.OPTION_N == 1 || p.OPTION_B == 1) printf("\n%6lu\t", n++);
+                if (p.OPTION_T == 1) {
+                  if (outputChar == '\t') {
+                    printf("%c%c", '^', 'I');
+                  }
+                }
+                // if (p.OPTION_S == 1) {
+                //   if ((outputChar = fgetc(p.p)) == '\n') {
+                // printf("%c", outputChar);
+                // while (outputChar == '\n') {
+                //   outputChar = fgetc(p.p);
+                // }
+                // printf("\n%c", outputChar);
+                // } else {
+                //   printf("\n%c", outputChar);
+                //   }
+                // }
+                if (p.OPTION_N == 1 || p.OPTION_E == 1) continue;
               }
-            }
-          } else if (p.OPTION_E == 1) {
-            while ((outputChar = fgetc(p.p)) != EOF) {
-              if (outputChar == '\n') printf("%c", '$');
+              printf("%c", outputChar);
+            } else {
               printf("%c", outputChar);
             }
-          } else {
-            while ((outputChar = fgetc(p.p)) != EOF) {
-              printf("%c", outputChar);
-            }
+
+            // if (p.OPTION_T == 1) {
+            //   if (outputChar == '\t') {
+            //     printf("%c%c", '^', 'I');
+            //     continue;
+            //   }
+            //   printf("%c", outputChar);
+            // }
+
+            // if (p.OPTION_S == 1) {
+            //   printf("%c", outputChar);
+            //   if (outputChar == '\n') {
+            //     if ((outputChar = fgetc(p.p)) == '\n') {
+            //       printf("%c", outputChar);
+            //       while (outputChar == '\n') {
+            //         outputChar = fgetc(p.p);
+            //       }
+            //       printf("%c", outputChar);
+            //     } else {
+            //       printf("%c", outputChar);
+            //     }
+            //   }
+            // }
+            // if (p.OPTION_E == 1) {
+            //   if (outputChar == '\n') printf("%c", '$');
+            //   printf("%c", outputChar);
+            // }
+            // else {
+            //   printf("%c", outputChar);
+            // }
           }
         }
         fclose(p.p);
